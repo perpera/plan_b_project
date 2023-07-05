fbb9be8bb6e1a5908645d074f3682db25ca46f54
perpera <131678085+perpera@users.noreply.github.com>
2023-07-05 13:38:50 +0300

    menu js to main js
    + updates modal form color

--- src/main.js
+++ src/main.js
@@ -1,12 +1,24 @@
 (() => {
   const refs = {
-    openModalBtn: document.querySelector('[data-modal-open]'),
+    openModalBtn: document.querySelectorAll('[data-modal-open]'),
     closeModalBtn: document.querySelector('[data-modal-close]'),
     modal: document.querySelector('[data-modal]'),
+    openMenuBtn: document.querySelector('[data-menu-open]'),
+    closeMenuBtn: document.querySelector('[data-menu-close]'),
+    menu: document.querySelector('[data-menu]'),
   };
 
-  refs.openModalBtn.addEventListener('click', toggleModal);
+  refs.openModalBtn.forEach(function (btn) {
+    btn.addEventListener('click', toggleModal);
+  });
   refs.closeModalBtn.addEventListener('click', toggleModal);
+  refs.openMenuBtn.addEventListener('click', toggleMenu);
+  refs.closeMenuBtn.addEventListener('click', toggleMenu);
+
+  function toggleMenu() {
+    refs.menu.classList.toggle('is-hidden');
+    document.body.classList.toggle('no-scroll');
+  }
 
   function toggleModal() {
     refs.modal.classList.toggle('is-hidden');
